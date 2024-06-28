dc764715396905c492552e695c9f0760f1de0ccb
YCLin mbp <a50206123@gmail.com>
2024-06-29 01:28:26 +0800

    Waiting Test

--- RCA001_main.py
+++ RCA001_main.py
@@ -24,7 +24,7 @@ from PyQt5.QtWidgets import *
 import time
 import numpy as np
 import pandas as pd
-import os, sys
+import os, sys, math
 
 
 global new_msg
@@ -117,15 +117,24 @@ def main() :
     if tie_demand == None :
         pass
     else :
-        new_msg_input('- 確認最小繫筋量 ...')
+        new_msg_input('\n- 確認最小繫筋量 ...')
         dbs = tie_demand_check(dbs, tie_demand, stir)
     
-    new_msg_input('- 開始順繫筋 ...')
+    new_msg_input('\n- 開始順繫筋 ...')
     for i in range(len(blocks)) :
         block = blocks[i]
-        new_msg_input('--  順柱 %s ' % block[0].split('"')[1])
+        new_msg_input('\n--  順柱 %s ' % block[0].split('"')[1])
         
         dbs = adjust_tie(dbs, block, tie_demand)
+    
+    new_msg_input('\n- B1F 柱筋繫滿 ...')
+    for i in range(len(dbs)) :
+        db = dbs[i]
+        col_name = db['col_name']
+        
+        if col_name[:3] == 'B1F' :
+            dbs[i] = adjust_B1F_col_tie(db)
+            
     
     new_msg_input('- 更新柱配筋 ...')
     rcol.rcol_db = dbs
@@ -181,6 +190,15 @@ def tie_demand_check(dbs, tie_demand, stirs) :
             tie_n = ties[n]
             nn = nxy[n]
             
+            # Tie Between per 2 rebar
+            if tie_n < math.floor((len(rebar[n])-2)/2) :
+                ties[n] = math.floor((len(rebar[n])-2)/2)
+                
+                rebar[n] = rd.modify_tie(rebar[n], ties[n])
+                
+                new_msg_input(f'--- 隔根箍調整!!!!! {col_name}, {dir[n]}向)')
+            
+            # Tie number excceeds the number of rebar
             if nn > tie_n : 
                 ties[n] = nn
                 if nn > (len(rebar[n])-2) :
@@ -218,22 +236,28 @@ def adjust_tie(dbs, block, tie_demand) :
         tie1 = list(db1['tie'])
         tie2 = list(db2['tie'])
         
+        stirNo1 = db1['stirrup'][0][0]
+        stirNo2 = db2['stirrup'][0][0]
+        
         dir = ['X', 'Y']
 
         #### Check No. of tie of below story is not less than one of upper story
-        for j in range(2) :
-            if tie2[j] < tie1[j] :
-                rebar = db2['rebar'][1][j]
-                
-                ## Check if tie is full, that it couldn't be changed.
-                tie2[j] = min(tie1[j], len(rebar) - 2)
-                
-                if tie2[j] == tie1[j] :
-                    new_msg_input(f'------ 繫筋調整 ({dir[j]}向)')
-                else :
-                    new_msg_input(f'------ 繫筋繫滿 ({dir[j]}向)')
+        if stirNo1 == stirNo2 :
+            for j in range(2) :
+                if tie2[j] < tie1[j] :
+                    rebar = db2['rebar'][1][j]
+                    
+                    ## Check if tie is full, that it couldn't be changed.
+                    tie2[j] = min(tie1[j], len(rebar) - 2)
+                    
+                    if tie2[j] == tie1[j] :
+                        new_msg_input(f'--- {db2["col_name"]} 繫筋調整 ({dir[j]}向)')
+                    else :
+                        new_msg_input(f'--- {db2["col_name"]} 繫筋繫滿 ({dir[j]}向)')
 
-                db2['rebar'][1][j] = rd.modify_tie(rebar, tie2[j])
+                    db2['rebar'][1][j] = rd.modify_tie(rebar, tie2[j])
+        else :
+            new_msg_input(f'--- {db2["col_name"]} 箍筋號數不同，不順繫筋')
         
         #### Check the tie number is the same if the setion is square
         width, height = db2['section']
@@ -251,15 +275,19 @@ def adjust_tie(dbs, block, tie_demand) :
     	#### Check if no tie when min. side >= 40cm
         if (min(width, height) >= 40.0) :
             for j in range(len(tie2)) :
-                rebar = db2['rebar'][1]
-                irebar = rebar.split()
+                rebar = db2['rebar'][1][j]
+                irebar = rebar[j]
                 if len(irebar) == 2 :
-                    irebar.append(irebar[i])
-                    rebar = ' '.join(irebar)
+                    irebar.append(irebar[1])
+                    irebar[1][0] = '1'
+                    db2['rebar'][1] = ' '.join(irebar)
+                    
 
-                tie2[j] = min[1, tie2[j]]
+                if tie2[j] < 1 :
+                    tie2[j] = 1
+                    db2['rebar'][1][j] = rd.modify_tie(rebar, tie2[j])
+                    new_msg_input(f'--- {db2["col_name"]} 柱寬超過40cm，繫筋至少1支調整')
 
-                new_msg_input(f'------ 柱寬超過40cm，繫筋至少1支調整')
     
         db2['tie'] = tuple(tie2)
         
@@ -270,6 +298,27 @@ def adjust_tie(dbs, block, tie_demand) :
         dbs[find_db_pos(dbs, story2+col_name)] = db2
     
     return dbs
+
+def adjust_B1F_col_tie(db) :
+    direction = ['X', 'Y']
+    rebar = db['rebar'][1]
+    tie = list(db['tie'])
+    col_name = db['col_name']
+    for i in range(len(rebar)) :
+        irebar = rebar[i]
+        itie = tie[i]
+        
+        max_tie = len(irebar) - 2
+        
+        if itie < max_tie :
+            db['rebar'][1][i] = rd.modify_tie(irebar, max_tie)
+            tie[i] = max_tie
+            
+            new_msg_input(f'-- {col_name} 柱筋繫滿 ({direction[i]}向)')
+            
+    db['tie'] = tuple(tie)
+
+    return db
 
 if __name__ == '__main__' :
     
